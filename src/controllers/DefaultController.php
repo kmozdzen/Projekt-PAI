5eb28cb81e0430adc2ef33a6fcdb64dc6720ebe6
kmozdzen <kmozdzen2@Onet.pl>
2022-11-14 18:47:01 +0100

    routing

---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController{
+    public function index()
+    {
+        $this->render('login');
+    }
+
+    public function search()
+    {
+        $this->render('search');
+    }
+}
